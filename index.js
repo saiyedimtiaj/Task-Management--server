f73c6c5553168afb53598c60d5b9ae9354ddf1a5
saiyedimtiaj <hridoyemtiaz0@gmail.com>
2023-12-22 12:40:17 +0600

    task status update anddelete sucessfully

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -47,6 +47,27 @@ async function run() {
     app.get('/alltask',async(req,res)=>{
       const email = req.query?.email
       const result = await taskColluction.find({email:email}).toArray()
+      res.send(result)
+    })
+
+    app.put('/alltask/:id',async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const status = req.body.status;
+      const updatedDoc = {
+        $set:{
+          status: status
+        }
+      }
+      const result = await taskColluction.updateOne(filter,updatedDoc,options)
+      res.send(result)
+    })
+
+    app.delete('/alltask/:id',async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}
+      const result = await taskColluction.deleteOne(filter)
       res.send(result)
     })
 
